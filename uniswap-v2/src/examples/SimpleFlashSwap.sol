959394ca4e4fb6f76926733cf22c95e50a478494
piatoss <crewe1746@naver.com>
2024-02-22 10:17:41 +0900

    Update SimpleFlashSwap

--- uniswap-v2/src/examples/SimpleFlashSwap.sol
+++ uniswap-v2/src/examples/SimpleFlashSwap.sol
@@ -6,34 +6,58 @@ import "../interfaces/IUniswapV2Pair.sol";
 import "../interfaces/IERC20.sol";
 
 contract SimpleFlashSwap is IUniswapV2Callee {
+    error OnlyOwner();
     error InvalidPair();
     error InvalidToken();
     error InvalidAmount();
+
+    address public owner;
 
     IUniswapV2Pair public immutable pair;
     address public immutable token0;
     address public immutable token1;
 
     constructor(IUniswapV2Pair _pair) {
+        owner = msg.sender;
         pair = _pair;
         token0 = _pair.token0();
         token1 = _pair.token1();
     }
 
+    function withdraw(address token, uint256 amount) external returns (bool) {
+        if (msg.sender != owner) {
+            revert OnlyOwner();
+        }
+
+        if (token != token0 && token != token1) {
+            revert InvalidToken();
+        }
+
+        return IERC20(token).transfer(owner, amount);
+    }
+
     function flashSwap(
         address tokenBorrow,
-        uint256 amount,
-        bool feeOn
+        address tokenRepay,
+        uint256 amount
     ) external {
         if (tokenBorrow != token0 && tokenBorrow != token1) {
             revert InvalidToken();
+        }
+
+        if (tokenRepay != token0 && tokenRepay != token1) {
+            revert InvalidToken();
+        }
+
+        if (amount == 0) {
+            revert InvalidAmount();
         }
 
         (uint amount0Out, uint amount1Out) = tokenBorrow == token0
             ? (amount, uint(0))
             : (uint(0), amount);
 
-        bytes memory data = encodeData(tokenBorrow, amount, feeOn);
+        bytes memory data = encodeData(tokenBorrow, tokenRepay, amount);
         pair.swap(amount0Out, amount1Out, address(this), data);
     }
 
@@ -47,9 +71,15 @@ contract SimpleFlashSwap is IUniswapV2Callee {
             revert InvalidPair();
         }
 
-        (address tokenBorrow, uint256 amount, bool feeOn) = decodeData(data);
+        (address tokenBorrow, address tokenRepay, uint256 amount) = decodeData(
+            data
+        );
 
         if (tokenBorrow != token0 && tokenBorrow != token1) {
+            revert InvalidToken();
+        }
+
+        if (tokenRepay != token0 && tokenRepay != token1) {
             revert InvalidToken();
         }
 
@@ -57,33 +87,62 @@ contract SimpleFlashSwap is IUniswapV2Callee {
             revert InvalidAmount();
         }
 
-        uint returnAmount = amount;
+        uint repayAmount;
 
-        if (feeOn) {
-            uint fee = calculateFee(amount);
-            returnAmount = amount + fee;
+        if (tokenBorrow == tokenRepay) {
+            repayAmount = repayWithSameToken(amount);
+        } else {
+            repayAmount = repayWithDifferentToken(tokenBorrow, amount);
         }
 
-        IERC20(tokenBorrow).transfer(address(pair), returnAmount);
+        IERC20(tokenRepay).transfer(address(pair), repayAmount);
     }
 
     function encodeData(
         address tokenBorrow,
-        uint256 amount,
-        bool feeOn
+        address tokenRepay,
+        uint256 amount
     ) public pure returns (bytes memory) {
-        return abi.encode(tokenBorrow, amount, feeOn);
+        return abi.encode(tokenBorrow, tokenRepay, amount);
     }
 
     function decodeData(
         bytes calldata data
-    ) public pure returns (address, uint256, bool) {
-        return abi.decode(data, (address, uint256, bool));
+    )
+        public
+        pure
+        returns (address tokenBorrow, address tokenRepay, uint256 amount)
+    {
+        return abi.decode(data, (address, address, uint256));
+    }
+    // about 0.3009% fee
+    function repayWithSameToken(
+        uint256 amount
+    ) public pure returns (uint256 repayAmount) {
+        repayAmount = (amount * 1000) / 997 + 1;
     }
 
-    // about 0.3% fee
-    function calculateFee(uint256 amount) public pure returns (uint256) {
-        return ((amount * 3) / 997) + 1;
+    function repayWithDifferentToken(
+        address tokenBorrow,
+        uint256 amount
+    ) public view returns (uint256 repayAmount) {
+        (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
+        (uint256 amount0Out, uint256 amount1Out) = tokenBorrow == token0
+            ? (amount, uint256(0))
+            : (uint256(0), amount);
+
+        uint numerator;
+        uint denominator;
+
+        if (amount0Out > 0) {
+            numerator = amount0Out * reserve1 * 1000;
+            denominator = (reserve0 - amount0Out) * 997;
+        } else {
+            numerator = amount1Out * reserve0 * 1000;
+            denominator = (reserve1 - amount1Out) * 997;
+        }
+
+        repayAmount = (numerator / denominator) + 1;
     }
 
     receive() external payable {}
--- uniswap-v2/test/SimpleFlashSwap.t.sol
+++ uniswap-v2/test/SimpleFlashSwap.t.sol
@@ -65,28 +65,103 @@ contract SimpleFlashSwapTest is Test {
 
         vm.label(address(flashSwap), "flashSwap");
 
-        token0.mint(address(flashSwap), 10000e18);
+        token0.mint(address(flashSwap), INIT_SUPPLY);
+        token1.mint(address(flashSwap), INIT_SUPPLY);
     }
 
-    function test_FlashSwap() public {
+    // ===== Repay with same token0 =====
+    function test_FlashSwapToken0ToToken1RepayWithToken0() public {
+        address tokenBorrow = address(token0);
+        address tokenRepay = address(token0);
         uint256 amount = 100000e18;
-        uint256 fee = flashSwap.calculateFee(amount);
-        uint256 swapBalanceBefore = token0.balanceOf(address(flashSwap));
-        uint256 pairBalanceBefore = token0.balanceOf(address(pair));
+        uint256 repayAmount = flashSwap.repayWithSameToken(amount);
+        uint256 fee = repayAmount - amount;
 
-        flashSwap.flashSwap(address(token0), amount, true);
+        uint256 flashToken0Before = token0.balanceOf(address(flashSwap));
+        uint256 pairToken0Before = token0.balanceOf(address(pair));
 
-        uint256 swapBalanceAfter = token0.balanceOf(address(flashSwap));
-        uint256 pairBalanceAfter = token0.balanceOf(address(pair));
+        flashSwap.flashSwap(tokenBorrow, tokenRepay, amount);
 
-        assertEq(swapBalanceAfter, swapBalanceBefore - fee);
-        assertEq(pairBalanceAfter, pairBalanceBefore + fee);
+        uint256 flashToken0After = token0.balanceOf(address(flashSwap));
+        uint256 pairToken0After = token0.balanceOf(address(pair));
+
+        assertEq(flashToken0After, flashToken0Before - fee);
+        assertEq(pairToken0After, pairToken0Before + fee);
     }
 
-    function test_RevertFlashSwapWithNoFee() public {
+    // ===== Repay with same token1 =====
+    function test_FlashSwapToken1ToToken0RepayWithToken1() public {
+        address tokenBorrow = address(token1);
+        address tokenRepay = address(token1);
         uint256 amount = 100000e18;
+        uint256 repayAmount = flashSwap.repayWithSameToken(amount);
+        uint256 fee = repayAmount - amount;
 
-        vm.expectRevert("UniswapV2: K");
-        flashSwap.flashSwap(address(token0), amount, false);
+        uint256 flashToken1Before = token1.balanceOf(address(flashSwap));
+        uint256 pairToken1Before = token1.balanceOf(address(pair));
+
+        flashSwap.flashSwap(tokenBorrow, tokenRepay, amount);
+
+        uint256 flashToken1After = token1.balanceOf(address(flashSwap));
+        uint256 pairToken1After = token1.balanceOf(address(pair));
+
+        assertEq(flashToken1After, flashToken1Before - fee);
+        assertEq(pairToken1After, pairToken1Before + fee);
+    }
+
+    // ===== Repay with different token1 =====
+    function test_FlashSwapToken0ToToken1RepayWithToken1() public {
+        address tokenBorrow = address(token0);
+        address tokenRepay = address(token1);
+        uint256 amount = 100000e18;
+        uint256 repayAmount = flashSwap.repayWithDifferentToken(
+            tokenBorrow,
+            amount
+        );
+
+        uint256 flashToken0Before = token0.balanceOf(address(flashSwap));
+        uint256 flashToken1Before = token1.balanceOf(address(flashSwap));
+        uint256 pairToken0Before = token0.balanceOf(address(pair));
+        uint256 pairToken1Before = token1.balanceOf(address(pair));
+
+        flashSwap.flashSwap(tokenBorrow, tokenRepay, amount);
+
+        uint256 flashToken0After = token0.balanceOf(address(flashSwap));
+        uint256 flashToken1After = token1.balanceOf(address(flashSwap));
+        uint256 pairToken0After = token0.balanceOf(address(pair));
+        uint256 pairToken1After = token1.balanceOf(address(pair));
+
+        assertEq(flashToken0After, flashToken0Before + amount);
+        assertEq(flashToken1After, flashToken1Before - repayAmount);
+        assertEq(pairToken0After, pairToken0Before - amount);
+        assertEq(pairToken1After, pairToken1Before + repayAmount);
+    }
+
+    // ===== Repay with different token0 =====
+    function test_FlashSwapToken1ToToken0RepayWithToken0() public {
+        address tokenBorrow = address(token1);
+        address tokenRepay = address(token0);
+        uint256 amount = 100000e18;
+        uint256 repayAmount = flashSwap.repayWithDifferentToken(
+            tokenBorrow,
+            amount
+        );
+
+        uint256 flashToken0Before = token0.balanceOf(address(flashSwap));
+        uint256 flashToken1Before = token1.balanceOf(address(flashSwap));
+        uint256 pairToken0Before = token0.balanceOf(address(pair));
+        uint256 pairToken1Before = token1.balanceOf(address(pair));
+
+        flashSwap.flashSwap(tokenBorrow, tokenRepay, amount);
+
+        uint256 flashToken0After = token0.balanceOf(address(flashSwap));
+        uint256 flashToken1After = token1.balanceOf(address(flashSwap));
+        uint256 pairToken0After = token0.balanceOf(address(pair));
+        uint256 pairToken1After = token1.balanceOf(address(pair));
+
+        assertEq(flashToken0After, flashToken0Before - repayAmount);
+        assertEq(flashToken1After, flashToken1Before + amount);
+        assertEq(pairToken0After, pairToken0Before + repayAmount);
+        assertEq(pairToken1After, pairToken1Before - amount);
     }
 }
